1021f584d3c9c7c4f34e27335f2b9008b176a0fb
Anthony Eli Agbenu <lefootballroi@yahoo.com>
2015-02-07 00:51:23 +0000

    updated the profile page

--- app/src/main/java/com/eliagbenu/switchdatingapp/controller/AppController.java
+++ app/src/main/java/com/eliagbenu/switchdatingapp/controller/AppController.java
@@ -13,6 +13,7 @@ public class AppController extends Application {
 
     final public static String PREF_NAME = "switch_pref";
     boolean signUpStatus;
+    final public static String API_TOKEN = "322f7d24ecfa9f7bff760ef755da75f8e8e3e288";
 
     final static public String API_URL = "https://theswitch.herokuapp.com/api/v1/";
 
--- app/src/main/java/com/eliagbenu/switchdatingapp/views/Signup.java
+++ app/src/main/java/com/eliagbenu/switchdatingapp/views/Signup.java
@@ -16,11 +16,17 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.eliagbenu.switchdatingapp.R;
 import com.eliagbenu.switchdatingapp.controller.AppController;
 import com.eliagbenu.switchdatingapp.utils.VolleySingleton;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -106,41 +112,28 @@ public class Signup extends ActionBarActivity {
 
         String url = AppController.API_URL+"rest-auth/registration/";
         // Get a RequestQueue
-        RequestQueue queue = VolleySingleton.getInstance(this.getApplicationContext()).
-                getRequestQueue();
+        JsonObject json = new JsonObject();
+        json.addProperty("username", username);
+        json.addProperty("password1", password);
+        json.addProperty("password2", password);
+        json.addProperty("email", email);
 
-        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>()
-                {
+        Ion.with(Signup.this)
+                .load(url)
+                .setHeader("Authorization ","Token "+ AppController.API_TOKEN)
+                .setJsonObjectBody(json)
+                .asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
                     @Override
-                    public void onResponse(String response) {
-                        // response
-                        Log.d("Response", response);
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // error
-                        Log.d("Error.Response", error.toString());
-                    }
-                }
-        ) {
-            @Override
-            protected Map<String, String> getParams()
-            {
-                Map<String, String>  params = new HashMap<String, String>();
+                    public void onCompleted(Exception e, JsonObject result) {
+                        if(e == null){
+                            return;
+                        }
 
-                params.put("token", "322f7d24ecfa9f7bff760ef755da75f8e8e3e288");
-                params.put("username", username);
-                params.put("email", email);
-                params.put("password", password);
 
-                return params;
-            }
-        };
-        queue.add(postRequest);
+                    }
+                });
+
 
     }
 
