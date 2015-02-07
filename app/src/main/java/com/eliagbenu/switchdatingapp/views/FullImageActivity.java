694f8f461667ed0b1aac8794dde4fc6d4742da0c
Anthony Eli Agbenu <lefootballroi@yahoo.com>
2015-02-07 04:50:18 +0000

    added like and pass buttons

--- app/src/main/java/com/eliagbenu/switchdatingapp/views/FullImageActivity.java
+++ app/src/main/java/com/eliagbenu/switchdatingapp/views/FullImageActivity.java
@@ -15,6 +15,7 @@ public class FullImageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_full_image);
 
         // get intent data
